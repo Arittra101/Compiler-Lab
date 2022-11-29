1a7aad18f6871a3ca27ec297f90caabcef3457df
Arittra101 <avishek51775@gmail.com>
2022-11-30 01:41:46 +0600

    update

--- assignment_01.cpp
+++ assignment_01.cpp
@@ -1,73 +1,76 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 typedef long long int lli;
-lli i,j;
+lli i, j;
 
-#define pii  pair<lli, lli>
+#define pii pair<lli, lli>
 #define vi vector<lli>
 #define pb push_back
 #define mp make_pair
-#define yes cout << "YES" << "\n";
-#define no cout << "NO" << "\n";
-#define SORT(v) sort(v.begin(),v.end());
-#define R_SORT(v) sort(v.begin(),v.end(),greater<lli>());
+#define yes cout << "YES" \
+                 << "\n";
+#define no cout << "NO" \
+                << "\n";
+#define SORT(v) sort(v.begin(), v.end());
+#define R_SORT(v) sort(v.begin(), v.end(), greater<lli>());
 #define en "\n"
-
 
 void solve()
 {
 
     string line;
-
+    lli cnt = 0;
     ofstream mylife2;
-    mylife2.open("pupils.txt");
-    ifstream myfile("cp.txt");
+    mylife2.open("pupils.txt"); // write
+    ifstream myfile("cp.txt");  // read
 
-    if(!myfile.is_open()) cout<<"Failed to Open"<<endl;
+    if (!myfile.is_open())
+        cout << "Failed to Open" << endl;
     else
     {
 
         bool samne_asbe = false;
         bool onelinecmnt = true;
-        while(getline(myfile,line))
+        while (getline(myfile, line))
         {
-            for(i=0; i<line.size(); i++)
+            cnt++;
+            for (i = 0; i < line.size(); i++)
             {
-                if(onelinecmnt == false)
+                if (onelinecmnt == false)
                 {
-                    samne_asbe =  false;
-                    onelinecmnt =  true;
+                    samne_asbe = false;
+                    onelinecmnt = true;
                 }
-                else if(line[0]=='/' && line[1]=='*' )
+                else if (line[0] == '/' && line[1] == '*')
                 {
-                    samne_asbe =  true;
+                    samne_asbe = true;
                     i++;
                 }
 
-                else if(line[0]=='/' && line[1]=='/')
+                else if (line[0] == '/' && line[1] == '/')
                 {
-                    samne_asbe =  true;
+                    samne_asbe = true;
                     onelinecmnt = false;
                 }
-                else if(samne_asbe==false)
+                else if (samne_asbe == false)
                 {
-                    cout<<line[i];
-                    mylife2<<line[i];
+                    if (line[i] != ' ')
+                    {
+                        cout << line[i];
+                        mylife2 << line[i];
+                    }
                 }
-                if(line[0]=='*' && line[1]=='/')
+                if (line[0] == '*' && line[1] == '/')
                 {
                     samne_asbe = false;
                     i++;
                 }
-
             }
-
         }
         myfile.close();
         mylife2.close();
     }
-
-
+    cout << cnt << endl;
 }
 int main()
 {
@@ -79,5 +82,3 @@ int main()
 
     return 0;
 }
-
-
